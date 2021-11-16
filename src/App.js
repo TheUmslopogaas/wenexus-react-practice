e4badff766437aa19234badf96c1319fc93d78c3
Nazmulhasan Kazal <nazmulhasankazal@gmail.com>
2021-11-17 02:40:40 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FirstComponent from "./my-components/FirstComponent";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <FirstComponent></FirstComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/my-components/FirstComponent.js
@@ -0,0 +1,7 @@
+function FirstComponent() {
+  return (
+    <div>Data from the first cutom component.</div>
+  )
+}
+
+export default FirstComponent;
